8def1b77b72c351086530237099563eb047afaa2
satyapsr13 <satyapsr13@gmail.com>
2024-01-29 10:15:13 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react' 
+import './App.css'
+// import './components/Login'
+import Login from './components/Login';
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <h1 className="text-3xl font-bold underline ">Hello world!</h1> */}
+      <Login/>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import loginImage from "../assets/bg1.jpg";
+
+const Login = () => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 bg-amber-300">
+      <img src={loginImage} alt="" />
+    </div>
+  );
+};
+
+export default Login;
